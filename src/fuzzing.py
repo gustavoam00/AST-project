a098ecc73314467eccfc8cd1d8b4b26e6d8d5af7
Tommy Ho <tho@student.ethz.ch>
2025-05-15 12:36:41 +0200

    fuzzing timeout fix

--- src/fuzzing.py
+++ src/fuzzing.py
@@ -287,7 +287,7 @@ def run_pipeline(init_cov: int, init_query: list, init_tables: list, init_nodes:
              queries.append(query[i:i+250])
 
         for q in queries:
-            lines_c, branch_c, taken_c, calls_c, msg = run_coverage(q, timeout=1)
+            lines_c, branch_c, taken_c, calls_c, msg = run_coverage(q, timeout=300)
             c = (lines_c, branch_c, taken_c, calls_c)
             cov = coverage_score(lines_c, branch_c, taken_c, calls_c)
 
