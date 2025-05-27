599ddaa9685ba5ef2867057f8ab8e30f1fff61db
Tommy Ho <tho@student.ethz.ch>
2025-05-27 20:54:19 +0200

    directories

--- src/config.py
+++ src/config.py
@@ -3,5 +3,5 @@ SQLITE_VERSIONS = ["sqlite3-3.26.0", "sqlite3-3.39.4"]
 DB1 = "./db/test1.db"
 DB2 = "./db/test2.db"
 BUGS_FOLDER = QUERY_TEST_FOLDER + "test2/" #"bugs/"
-INFO_OUTPUT = QUERY_TEST_FOLDER + "test/"
-TEMP_OUTPUT = QUERY_TEST_FOLDER + "test2/"
+INFO_OUTPUT = QUERY_TEST_FOLDER + "info/"
+TEMP_OUTPUT = QUERY_TEST_FOLDER + "temp/"
