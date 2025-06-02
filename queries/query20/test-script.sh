5a08996ece86fc7f25a2a7adab49a7ce184b4f06
Tommy Ho <tho@student.ethz.ch>
2025-06-02 12:45:45 +0200

    fix TEST_CASE_LOCATION scripts and reducer

--- queries/query20/test-script.sh
+++ queries/query20/test-script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-DEFAULT_PATH="$SCRIPT_DIR/query.sql"
-FILE_PATH="${TEST_CASE_LOCATION:-$DEFAULT_PATH}"
+DEFAULT_PATH="$SCRIPT_DIR"
+FILE_PATH="${TEST_CASE_LOCATION:-$DEFAULT_PATH}/query.sql"
 
 python3 src/test_bug.py --query "$FILE_PATH" --oracle "CRASH(3.26.0)"
