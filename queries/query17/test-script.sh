544c744776d667145a84f86021d3767c2c139361
Tommy Ho <tho@student.ethz.ch>
2025-06-02 15:20:04 +0200

    test script fix

--- queries/query17/test-script.sh
+++ queries/query17/test-script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-DEFAULT_PATH="$SCRIPT_DIR"
-FILE_PATH="${TEST_CASE_LOCATION:-$DEFAULT_PATH}/query.sql"
+DEFAULT_PATH="$SCRIPT_DIR/query.sql"
+FILE_PATH="${TEST_CASE_LOCATION:-$DEFAULT_PATH}"
 
 python3 src/test_bug.py --query "$FILE_PATH" --oracle "DIFF" --notfull
