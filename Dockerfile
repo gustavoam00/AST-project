0e68197697f408c5cb062d0f862a1de84af6ad8d
Tommy Ho <tho@student.ethz.ch>
2025-06-04 18:05:44 +0200

    dockerfile and zip

--- Dockerfile
+++ Dockerfile
@@ -2,13 +2,17 @@ FROM theosotr/sqlite3-test
 
 RUN sudo apt update && \
     sudo apt install -y python3 python3-pip && \
+    sudo apt install -y python3 python3-pip unzip && \
     pip3 install tqdm
 
 WORKDIR /app
 
-COPY . .
+COPY source_code.zip .
+COPY reducer .
 
 USER root
+RUN unzip source_code.zip && rm source_code.zip && \
+    find /app/queries -name "*.sh" -exec chmod +x {} \;
 RUN sed -i 's/\r//' /app/reducer
 RUN chmod +x /app/reducer && mv /app/reducer /usr/bin/reducer
 
---
+++ source_code.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+
+ZIP_NAME="source_code.zip"
+QUERIES_TEST="queries"
+DB="db"
+SOURCE_DIR="src"
+MAIN_FILE="main.py"
+
+# Clean up any existing zip file
+[ -f "$ZIP_NAME" ] && rm "$ZIP_NAME"
+
+# Check that required files exist
+[ ! -d "$QUERIES_TEST" ] && echo "Directory '$QUERIES_TEST' not found!" && exit 1
+[ ! -d "$SOURCE_DIR" ] && echo "Directory '$SOURCE_DIR' not found!" && exit 1
+[ ! -f "$MAIN_FILE" ] && echo "File '$MAIN_FILE' not found!" && exit 1
+
+# Add main.py and src/ folder into the zip
+zip -r "$ZIP_NAME" "$MAIN_FILE" "$SOURCE_DIR" "$QUERIES_TEST" "$DB"
+
+echo "✅ Created $ZIP_NAME with 'main.py' and '$SOURCE_DIR/'"
